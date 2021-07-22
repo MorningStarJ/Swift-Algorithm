709dbaad652ee529a1b4107312bbb96116a24531
MorningStar <jngviho@icloud.com>
2021-07-22 22:07:18 +0800

    Stack&Queue.LeetCode-225. Implement Stack using Queues

---
+++ Category/Stack/225.ImplementStackUsingQueues.playground/Contents.swift
@@ -0,0 +1,71 @@
+import Foundation
+
+//225. Implement Stack using Queues
+//Implement a last-in-first-out (LIFO) stack using only two queues. The implemented stack should support all the functions of a normal stack (push, top, pop, and empty).
+//
+//Implement the MyStack class:
+//
+//void push(int x) Pushes element x to the top of the stack.
+//int pop() Removes the element on the top of the stack and returns it.
+//int top() Returns the element on the top of the stack.
+//boolean empty() Returns true if the stack is empty, false otherwise.
+//Notes:
+//
+//You must use only standard operations of a queue, which means that only push to back, peek/pop from front, size and is empty operations are valid.
+//Depending on your language, the queue may not be supported natively. You may simulate a queue using a list or deque (double-ended queue) as long as you use only a queue's standard operations.
+//
+//
+//Example 1:
+//
+//Input
+//["MyStack", "push", "push", "top", "pop", "empty"]
+//[[], [1], [2], [], [], []]
+//Output
+//[null, null, null, 2, 2, false]
+//
+//Explanation
+//MyStack myStack = new MyStack();
+//myStack.push(1);
+//myStack.push(2);
+//myStack.top(); // return 2
+//myStack.pop(); // return 2
+//myStack.empty(); // return False
+//
+//
+//Constraints:
+//
+//1 <= x <= 9
+//At most 100 calls will be made to push, pop, top, and empty.
+//All the calls to pop and top are valid.
+//
+//
+//Follow-up: Can you implement the stack using only one queue?
+class MyStack {
+    
+    var array: [Int] = []
+
+    /** Initialize your data structure here. */
+    init() {
+
+    }
+    
+    /** Push element x onto stack. */
+    func push(_ x: Int) {
+        array.append(x)
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    func pop() -> Int {
+        array.popLast() ?? -1
+    }
+    
+    /** Get the top element. */
+    func top() -> Int {
+        array.last ?? -1
+    }
+    
+    /** Returns whether the stack is empty. */
+    func empty() -> Bool {
+        array.isEmpty
+    }
+}
