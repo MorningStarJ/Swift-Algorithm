6c7ba0db9857eb6adcbb9f6eb083027309d9d81e
MorningStar <jngviho@icloud.com>
2021-07-29 22:33:03 +0800

    Stack&Queue.LeetCode-145. Binary Tree Postorder Traversal

---
+++ Category/BinaryTree/145.BinaryTreePostorderTraversal.playground/Contents.swift
@@ -0,0 +1,69 @@
+import Foundation
+
+//145. Binary Tree Postorder Traversal
+//Given the root of a binary tree, return the postorder traversal of its nodes' values.
+//
+// 
+//
+//Example 1:
+//
+//
+//Input: root = [1,null,2,3]
+//Output: [3,2,1]
+//Example 2:
+//
+//Input: root = []
+//Output: []
+//Example 3:
+//
+//Input: root = [1]
+//Output: [1]
+//Example 4:
+//
+//
+//Input: root = [1,2]
+//Output: [2,1]
+//Example 5:
+//
+//
+//Input: root = [1,null,2]
+//Output: [2,1]
+// 
+//
+//Constraints:
+//
+//The number of the nodes in the tree is in the range [0, 100].
+//-100 <= Node.val <= 100
+// 
+//
+//Follow up: Recursive solution is trivial, could you do it iteratively?
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init(_ val: Int = 0,
+                _ left: TreeNode? = nil,
+                _ right: TreeNode? = nil) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+class Solution {
+    func postorderTraversal(_ root: TreeNode?) -> [Int] {
+        var result: [Int] = []
+        traversePostOrder(current: root) { value in
+            result.append(value)
+        }
+        return result
+    }
+    
+    public func traversePostOrder(current: TreeNode?, visit: (Int) -> Void) {
+        guard let cur = current else { return }
+        
+        traversePostOrder(current: cur.left, visit: visit)
+        traversePostOrder(current: cur.right, visit: visit)
+        visit(cur.val)
+    }
+}
